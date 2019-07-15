2d31da7babf86792d03bd6ca1d211cd5a025a5c1
David McAlpine <dave@mcalpine.io>
2019-07-14 22:36:06 -0400

    added localstorage support

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import Clock from './Clock'
 import allTimeZones from './timezones'
 
-function App() {
+const TIME_ZONE_KEY = "timeZones"
 
+function App() {
+  let defaultTimeZones = localStorage.getItem(TIME_ZONE_KEY)
+  defaultTimeZones = defaultTimeZones ? JSON.parse(defaultTimeZones) : []
+  
   // myTimeZones: a list of time zones i've selected to keep track of
   // selectedTimeZone: the time zone to be added to myTimeZones
-  const [myTimeZones, setMyTimeZones] = useState([])
+  const [myTimeZones, setMyTimeZones] = useState(defaultTimeZones)
   const [selectedTimeZone, setSelectedTimeZone] = useState('')
 
   // Event handler for selecting a new time zone
@@ -23,17 +27,18 @@ function App() {
   // Event handler for adding a new time zone
   function handleAdd() {
     if (selectedTimeZone) {
-      setMyTimeZones([...myTimeZones, selectedTimeZone])
+      let newTimeZones = [...myTimeZones, selectedTimeZone]
+      setMyTimeZones(newTimeZones)
+      localStorage.setItem(TIME_ZONE_KEY, JSON.stringify(newTimeZones))
     }
   }
 
   // Event hanlder for removing a time zone
   function handleRemove(timeZone){
-    console.log('removing', timeZone)
     let index = myTimeZones.findIndex(tz => timeZone === tz)
     myTimeZones.splice(index, 1)
-    console.log(index, myTimeZones)
     setMyTimeZones([...myTimeZones])
+    localStorage.setItem(TIME_ZONE_KEY, JSON.stringify([...myTimeZones]))
   }
 
   return (
